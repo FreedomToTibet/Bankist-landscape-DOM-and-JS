5ba897848e3d7833f0db4176de163439b33e3fff
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2023-11-10 19:59:42 +0100

    tabs menu animation sticky menu

--- script.js
+++ script.js
@@ -49,4 +49,74 @@ btnScrollTo.addEventListener('click', e => {section1.scrollIntoView({behavior: '
 //   });
 // });
 
+///////////////////////////////////////
+// Navigation
 
+document.querySelector('.nav__links').addEventListener('click', e => {
+	e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
+});
+
+///////////////////////////////////////
+// Tabbed component
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+
+tabsContainer.addEventListener('click', (e) => {
+	const clicked = e.target.closest('.operations__tab');
+
+	if (!clicked) return;
+
+	tabsContainer.querySelector('.operations__tab--active').classList.remove('operations__tab--active');
+	clicked.classList.add('operations__tab--active');
+
+	document.querySelector('.operations__content--active').classList.remove('operations__content--active');
+	document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+});
+
+///////////////////////////////////////
+// Menu fade animation
+
+const nav = document.querySelector('.nav');
+
+const handleNavHover = (opacity) => (e) => {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+}
+
+nav.addEventListener('mouseover', handleNavHover(0.5));
+nav.addEventListener('mouseout', handleNavHover(1));
+
+///////////////////////////////////////
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const headerObserver = new IntersectionObserver(
+	(entries) => {
+		const [entry] = entries;
+		
+		if (!entry.isIntersecting) nav.classList.add('sticky');
+		else nav.classList.remove('sticky');
+	}
+	, {
+		root: null,
+		threshold: 0,
+		rootMargin: `-${navHeight}px`,
+	}
+);
+
+headerObserver.observe(header);
