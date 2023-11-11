5419ce6dd462de996416365146eec7ec47d766c2
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2023-11-11 23:44:27 +0100

    reveal sections

--- script.js
+++ script.js
@@ -120,3 +120,39 @@ const headerObserver = new IntersectionObserver(
 );
 
 headerObserver.observe(header);
+
+///////////////////////////////////////
+// Logo click for scrolling to the top
+
+const logoOnTop = document.querySelector('#logo');
+
+logoOnTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+///////////////////////////////////////
+// Reveal sections
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+	const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+	sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
